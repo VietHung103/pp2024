127248185ec7bcb0bffa018aa4f7cec2342e46e9
VietHung103 <viethungpc103@gmail.com>
2024-12-12 10:18:46 +0700

    prac1_2funtion

---
+++ studentmanagement.py
@@ -0,0 +1,13 @@
+class Student:
+    def __init__(self, student_id, name, dob,):
+        self.student_id = student_id
+        self.name = name
+        self.dob = dob
+        self.mark = {}
+
+class Course:
+    def __init__(self, course_id, name):
+        self.course_id = course_id
+        self.name = name
+        
+
